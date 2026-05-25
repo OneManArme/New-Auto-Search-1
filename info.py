705bfe382a422d3b8921cb7a909d625782683b68
OneManArme <asuran1manarmy@gmail.com>
2026-05-25 23:39:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -57,11 +57,11 @@ OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/moviesearchgrp')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/moviesearchgrp')
 
 AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-1003645978156") # add multiple channels here, separated by single space
-AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '-100')
+AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '0')
 
 IS_VERIFY = is_enabled(environ.get('IS_VERIFY', "True"), True)
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1003999417050'))
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1003999417050'))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '0'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/demoshort/81")
